f565f7cdeb645b43e6c6244aae88790ef425c4c3
May <mayvangsgaard@gmail.com>
2025-03-26 13:56:14 +0100

    ✏️ Opdateret tekst i page.js

--- app/page.js
+++ app/page.js
@@ -63,7 +63,7 @@ const riskColorMap = {
         {/* Input Area */}
         <textarea
           className="w-full p-4 border border-gray-300 rounded-lg text-lg focus:ring-2 focus:ring-blue-500 mt-2 shadow-sm placeholder:text-center"
-          placeholder="Kopier tekst og indsæt her."
+          placeholder="Kopier tekst og indsæt her"
           value={messageText}
           onChange={(e) => setMessageText(e.target.value)}
         ></textarea>
@@ -90,7 +90,7 @@ const riskColorMap = {
               </div>
 
               {/* Risk level label (text) */}
-              <div className="flex items-end pb-1">
+              <div className="flex items-end pb-5">
                 <span className="text-lg font-semibold">{riskLevel}</span>
               </div>
             </div>
